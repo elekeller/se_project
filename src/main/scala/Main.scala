a5b3ac0d085a955079fc5230bf68ad4053b26efe
elekeller <115349982+elekeller@users.noreply.github.com>
2024-04-05 08:37:27 +0200

    Introduce Cards
    Cards have a color and a type

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,3 +3,17 @@
   println(msg)
 
 def msg = "I was compiled by Scala 3. :)"
+
+sealed trait Color
+case object Red extends Color
+case object Green extends Color
+case object Blue extends Color
+case object Yellow extends Color
+
+sealed trait CardType
+case class Point(value:Int) extends CardType
+case class Draw2Card extends CardType
+case class SkipCard extends CardType
+case class ReverseCard extends CardType
+case class WildCard extends CardType
+case class WildDraw4Card extends CardType
